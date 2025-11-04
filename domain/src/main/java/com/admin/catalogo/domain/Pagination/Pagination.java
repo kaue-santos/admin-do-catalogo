414dfdae9963ecb3f1aeb7843c793676ac6bcc00
kaue-santos <kauemurilodossantos@gmail.com>
2025-11-04 19:19:49 -0300

    feat: criando CategoryGateway

---
+++ domain/src/main/java/com/admin/catalogo/domain/Pagination/Pagination.java
@@ -0,0 +1,11 @@
+package com.admin.catalogo.domain.Pagination;
+
+import java.util.List;
+
+public record Pagination<T>(
+        int currentPage,
+        int perPage,
+        long total,
+        List<T> list
+) {
+}
---
+++ domain/src/main/java/com/admin/catalogo/domain/category/CategoryGateway.java
@@ -0,0 +1,18 @@
+package com.admin.catalogo.domain.category;
+
+import com.admin.catalogo.domain.Pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    void deleteById(CategoryID anId);
+
+    Optional<Category> findById(CategoryID anId);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+}
