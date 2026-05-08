6111c8d0c532c8010e0f7be34e9e2d5d579893a1
kauesantos <kauemurilodossantos@gmail.com>
2026-05-08 13:13:34 -0300

    feat: update genre application

---
+++ application/src/main/java/com/admin/catalogo/application/genre/update/UpdateGenreOutput.java
@@ -0,0 +1,10 @@
+package com.admin.catalogo.application.genre.update;
+
+import com.admin.catalogo.domain.genre.Genre;
+
+public record UpdateGenreOutput(String id) {
+
+    public static UpdateGenreOutput from(final Genre aGenre) {
+        return new UpdateGenreOutput(aGenre.getId().getValue());
+    }
+}
---
+++ application/src/main/java/com/admin/catalogo/application/genre/update/UpdateGenreUseCase.java
@@ -0,0 +1,6 @@
+package com.admin.catalogo.application.genre.update;
+
+import com.admin.catalogo.application.UseCase;
+
+public abstract class UpdateGenreUseCase extends UseCase<UpdateGenreCommand, UpdateGenreOutput> {
+}
