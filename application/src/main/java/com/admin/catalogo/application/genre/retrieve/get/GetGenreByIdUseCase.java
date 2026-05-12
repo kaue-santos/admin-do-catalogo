25e3b61a49b751c1de66eb863dc12bd8021b22f0
kauesantos <kauemurilodossantos@gmail.com>
2026-05-11 21:40:25 -0300

    feat: get by id genre application

---
+++ application/src/main/java/com/admin/catalogo/application/genre/retrieve/get/GetGenreByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.admin.catalogo.application.genre.retrieve.get;
+
+import com.admin.catalogo.application.UseCase;
+
+public abstract class GetGenreByIdUseCase extends UseCase<String, GenreOutput> {
+}
