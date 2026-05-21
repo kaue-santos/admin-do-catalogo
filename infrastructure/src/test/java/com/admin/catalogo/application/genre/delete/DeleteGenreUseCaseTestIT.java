710dd8535b65a4daa50dc977d9ea5ff01e6f7f85
kauesantos <kauemurilodossantos@gmail.com>
2026-05-21 17:50:34 -0300

    test: genre delete it

---
+++ infrastructure/src/test/java/com/admin/catalogo/application/genre/delete/DeleteGenreUseCaseTestIT.java
@@ -0,0 +1,55 @@
+package com.admin.catalogo.application.genre.delete;
+
+import com.admin.catalogo.IntegrationTest;
+import com.admin.catalogo.domain.genre.Genre;
+import com.admin.catalogo.domain.genre.GenreGateway;
+import com.admin.catalogo.domain.genre.GenreID;
+import com.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class DeleteGenreUseCaseTestIT {
+
+    @Autowired
+    private DeleteGenreUseCase useCase;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Test
+    public void givenAValidGenreId_whenCallsDeleteGenre_shouldBeDeleteGenre() {
+        //given
+        final var aGenre = genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = aGenre.getId();
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        //when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        //then
+        Assertions.assertEquals(0, genreRepository.count());
+    }
+
+    @Test
+    public void givenAnInvalidGenreId_whenCallsDeleteGenre_shouldBeOk() {
+        //given
+        final var aGenre = genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = GenreID.from("123");
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        //when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        //then
+        Assertions.assertEquals(1, genreRepository.count());
+    }
+}
