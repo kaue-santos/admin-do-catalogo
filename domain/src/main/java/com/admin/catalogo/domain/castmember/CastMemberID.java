bc48ddfad8bc445ef04f0c0242d78304880eb6e5
kauesantos <kauemurilodossantos@gmail.com>
2026-05-29 18:33:13 -0300

    feat: estrutura inicial castmember

---
+++ domain/src/main/java/com/admin/catalogo/domain/castmember/CastMemberID.java
@@ -0,0 +1,44 @@
+package com.admin.catalogo.domain.castmember;
+
+import com.admin.catalogo.domain.Identifier;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CastMemberID extends Identifier {
+
+    private final String value;
+
+    private CastMemberID(final String anId) {
+        Objects.requireNonNull(anId);
+        this.value = anId;
+    }
+
+    public static CastMemberID unique(){
+        return CastMemberID.from(UUID.randomUUID());
+    }
+    public static CastMemberID from (final String anId){
+        return new CastMemberID(anId);
+    }
+    public static CastMemberID from (final UUID anId){
+        return new CastMemberID(anId.toString().toLowerCase());
+    }
+
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CastMemberID that = (CastMemberID) o;
+        return Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getValue());
+    }
+
+}
---
+++ domain/src/main/java/com/admin/catalogo/domain/castmember/CastMemberType.java
@@ -0,0 +1,6 @@
+package com.admin.catalogo.domain.castmember;
+
+public enum CastMemberType
+{
+    ACTOR, DIRECTOR;
+}
