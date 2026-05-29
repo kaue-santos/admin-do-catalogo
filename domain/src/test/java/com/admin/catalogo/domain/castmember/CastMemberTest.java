d23932b0df670e4f95858da8db0108291089e449
kauesantos <kauemurilodossantos@gmail.com>
2026-05-29 18:33:23 -0300

    test: estrutura inicial castmember

---
+++ domain/src/test/java/com/admin/catalogo/domain/castmember/CastMemberTest.java
@@ -0,0 +1,98 @@
+package com.admin.catalogo.domain.castmember;
+
+import com.admin.catalogo.domain.exceptions.NotificationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CastMemberTest {
+
+    @Test
+    public void givenAValidParams_whenCallsNewMemberThanACastMember()
+    {
+        final var expectedName = "Vin Diesel";
+        final var expectedType = CastMemberType.ACTOR;
+
+        final var actualMember = CastMember.newMember(expectedName, expectedType);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+        Assertions.assertEquals(expectedName, actualMember.getName());
+        Assertions.assertEquals(expectedType, actualMember.getType());
+        Assertions.assertNotNull(actualMember.getCreatedAt());
+        Assertions.assertNotNull(actualMember.getUpdatedAt());
+        Assertions.assertEquals(actualMember.getUpdatedAt(), actualMember.getCreatedAt());
+    }
+
+    @Test
+    public void givenAInvalidNullName_whenCallsNewMember_shouldReceiveANotification () {
+        final String expectedName = null;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidEmptyName_whenCallsNewMember_shouldReceiveANotification () {
+        final String expectedName = "";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidNameWithLengthMoreThan255_whenCallsNewMember_shouldReceiveANotification () {
+        final var expectedName = """
+                O incentivo ao avanço tecnológico, assim como a valorização de fatores subjetivos desafia a capacidade 
+                de equalização do sistema de formação de quadros que corresponde às necessidades.
+                O incentivo ao avanço tecnológico, assim como a valorização de fatores subjetivos desafia a capacidade 
+                de equalização do sistema de formação de quadros que corresponde às necessidades.
+                """;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidNullType_whenCallsNewMember_shouldReceiveANotification () {
+        final var expectedName = "Vin Diesel";
+        final CastMemberType expectedType = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var actualException = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+}
